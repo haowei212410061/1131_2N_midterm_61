e10734a916b6131f5bcab59c26d60ad317b38f33
haowei <212410061@365.tku.edu.tw>
2024-11-06 21:42:41 +0800

    update3

--- src/App_61.jsx
+++ src/App_61.jsx
@@ -7,7 +7,7 @@ import BlogNodePage_xx from './pages/BlogNodePage_xx'
 import BlogSupaPage2_xx from './pages/BlogSupaPage2_xx'
 
 import MidP1Page_61 from './pages/mid_61/P1Page_61'
-// import MidP2Page_61 from './pages/mid_61/P2Page_61'
+import MidP2Page_61 from './pages/mid_61/P2Page_61'
 // import MidP3Page_xx from './pages/mid_61/P3Page_61'
 
 const App_61 = () => {
@@ -20,7 +20,8 @@ const App_61 = () => {
         <Route path='/supa2_xx' element={<BlogSupaPage2_xx />} />
       </Routes>
       <Routes>
-        <Route path='/mid_61/p3_61' element={<MidP1Page_61 />} />
+        <Route path='/mid_61/p1_61' element={<MidP1Page_61 />} />
+        <Route path='/mid_61/p3_61' element={<MidP2Page_61 />} />
       </Routes>
     </BrowserRouter>
   )
