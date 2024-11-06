2380d98a9e6d1539e38b9e3f235fbdfdd15545ac
haowei <212410061@365.tku.edu.tw>
2024-11-06 21:39:19 +0800

    update

--- src/App_61.jsx
+++ src/App_61.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage_xx from './pages/HomePage_xx'
 import BlogStaticPage_xx from './pages/BlogStaticPage_xx'
 import BlogNodePage_xx from './pages/BlogNodePage_xx'
-import BlogSupaPage_xx from './pages/BlogSupaPage_xx'
+// import BlogSupaPage_xx from './pages/BlogSupaPage_xx'
 import BlogSupaPage2_xx from './pages/BlogSupaPage2_xx'
 
 import MidP1Page_61 from './pages/mid_61/P1Page_61'
-import MidP2Page_61 from './pages/mid_61/P2Page_61'
-import MidP3Page_xx from './pages/mid_61/P3Page_61'
+// import MidP2Page_61 from './pages/mid_61/P2Page_61'
+// import MidP3Page_xx from './pages/mid_61/P3Page_61'
 
 const App_61 = () => {
   return (
@@ -20,7 +20,7 @@ const App_61 = () => {
         <Route path='/supa2_xx' element={<BlogSupaPage2_xx />} />
       </Routes>
       <Routes>
-        <Route path='/mid_61/p3_61' element={<MidP1Page_61 />} />
+        <Route path='/mid_61/p_61' element={<MidP1Page_61 />} />
       </Routes>
     </BrowserRouter>
   )
